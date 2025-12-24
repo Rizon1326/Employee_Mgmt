2f617a00234ca8c30c46ca2dab5cae02b665fe14
Rizon1326 <bsse1326@iit.du.ac.bd>
2025-12-24 23:40:04 +0600

    refactor: Update City component layout to include CreateCity management card; enhance UI structure

--- emp_mgmt_frontend/src/features/adminSettings/components/cities/City.tsx
+++ emp_mgmt_frontend/src/features/adminSettings/components/cities/City.tsx
@@ -1,16 +1,22 @@
 import ManagementCard from "../ManagementCard";
 import { CityList } from "./CityList";
+import { CreateCity } from "./CreateCity";
 export const City = () => {
   const isActiveSection = true;
   return (
     <>
+       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
         {isActiveSection && (
           <>
             <ManagementCard title="Cities List">
               <CityList />
             </ManagementCard>
+            <ManagementCard title="Add Cities">
+              <CreateCity/>
+            </ManagementCard>
           </>
         )}
+       </div>
     </>
   );
 };
