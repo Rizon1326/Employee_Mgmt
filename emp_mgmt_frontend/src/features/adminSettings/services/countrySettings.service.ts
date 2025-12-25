96787e80d5a0f660a4cea590bda7d87671e2a4e3
Rizon1326 <bsse1326@iit.du.ac.bd>
2025-12-25 16:07:49 +0600

    refactor: Remove unused CountryList and CreateCountry components; implement country settings service and types

---
+++ emp_mgmt_frontend/src/features/adminSettings/services/countrySettings.service.ts
@@ -0,0 +1,20 @@
+import { fetchList,updateItem,createItem,deleteItem } from "../../../api/apiClient";
+import type { Country, CreateCountry } from "../types/countrySettings";
+
+export const fetchCountries = async ():Promise<Country[]> =>{
+   return fetchList<Country>("countries");
+}
+
+export const createCountry = async (countryData: Partial<CreateCountry>): Promise<CreateCountry> => {
+  return createItem<CreateCountry>("countries", countryData);
+}
+
+
+export const updateCountry = async (id: number, countryData: Partial<CreateCountry>): Promise<CreateCountry> => {
+  return updateItem<CreateCountry>("cities", id, countryData);
+};
+
+export const deleteCountry = async (id:number):Promise<void>=>{
+    return deleteItem("countries",id);
+}
+
---
+++ emp_mgmt_frontend/src/features/adminSettings/types/countrySettings.ts
@@ -0,0 +1,7 @@
+export type Country = {
+  id: number;
+  name: string;
+};
+export type CreateCountry ={
+    "name":string;
+}
